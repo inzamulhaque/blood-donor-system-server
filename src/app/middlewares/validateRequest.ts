14a4eac1c2bc38925ea9f03b0eafd370fef22896
inzamulhaque <mdihalif@gmail.com>
2025-10-23 10:50:01 +0600

    create validate request middleware and catchAsync util

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,16 @@
+import { ZodObject } from "zod";
+import catchAsync from "../utils/catchAsync";
+import type { NextFunction, Request, Response } from "express";
+
+const validateRequest = (schema: ZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await schema.parseAsync({
+      body: req.body,
+      cookies: req.cookies,
+    });
+
+    next();
+  });
+};
+
+export default validateRequest;
