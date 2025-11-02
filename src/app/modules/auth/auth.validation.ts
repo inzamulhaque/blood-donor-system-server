d5ba377ea1ec0d6d3736f584f1c358ad9efda785
inzamulhaque <mdihalif@gmail.com>
2025-11-02 23:04:11 +0600

    fix signin validation schema

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const SignInValidationSchema = z.object({
   body: z.object({
-    email: z.string().email("Invalid email address"),
+    email: z.templateLiteral([z.string().min(1), "@", z.string().max(64)]),
     password: z
       .string()
       .min(6, "Password must be at least 6 characters long")
