281acb2e601df1b79bf88de289d110359e6412da
inzamulhaque <mdihalif@gmail.com>
2025-11-03 17:22:28 +0600

    create finder schema

--- src/app/modules/finder/finder.interface.ts
+++ src/app/modules/finder/finder.interface.ts
@@ -1,6 +1,9 @@
+import type { TUpozila } from "../donor/donor.interface";
+
 export interface IFinder {
   name: string;
   email: string;
   phone: string;
-  password: string;
+  trackingNumber: number;
+  upozila: TUpozila;
 }
---
+++ src/app/modules/finder/finder.model.ts
@@ -0,0 +1,42 @@
+import { model, Schema } from "mongoose";
+import type { IFinder } from "./finder.interface";
+import { UPOZILAS_PABNA } from "../donor/donor.constant";
+
+const finderSchema = new Schema<IFinder>(
+  {
+    name: {
+      type: String,
+      required: [true, "Finder name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Finder email is required"],
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Contact number is required"],
+      trim: true,
+    },
+    trackingNumber: {
+      type: Number,
+      required: [true, "Tracking number is required"],
+      minlength: 6,
+      maxlength: 6,
+    },
+    upozila: {
+      type: String,
+      required: [true, "Upozila is required"],
+      enum: UPOZILAS_PABNA,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Finder = model<IFinder>("Finder", finderSchema);
+
+export default Finder;
