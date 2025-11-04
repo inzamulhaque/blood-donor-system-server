668245e63b115aad403bc6f714c54c3612d3d8d4
inzamulhaque <mdihalif@gmail.com>
2025-11-04 20:26:22 +0600

    create get me controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,7 +1,12 @@
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import httpsStatus from "http-status";
-import { createNewDonorService, createNewFinderService } from "./user.services";
+import {
+  createNewDonorService,
+  createNewFinderService,
+  getMeService,
+} from "./user.services";
+import type { JwtPayload } from "jsonwebtoken";
 
 export const createNewDonor = catchAsync(async (req, res) => {
   const result = await createNewDonorService(req.body);
@@ -21,6 +26,19 @@ export const createNewFinder = catchAsync(async (req, res) => {
     statusCode: httpsStatus.CREATED,
     success: true,
     message: "New finder created successfully!",
+    data: result,
+  });
+});
+
+export const getMe = catchAsync(async (req, res) => {
+  const user = req?.user;
+
+  const result = await getMeService(user as JwtPayload);
+
+  sendResponse(res, {
+    statusCode: httpsStatus.OK,
+    success: true,
+    message: "Get my information successfully!",
     data: result,
   });
 });
