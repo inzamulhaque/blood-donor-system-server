f96d096360c27a09b1a023094cf6d350ab18c330
inzamulhaque <mdihalif@gmail.com>
2025-11-05 15:49:23 +0600

    create update password validation schema

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -9,3 +9,23 @@ export const SignInValidationSchema = z.object({
       .max(18, "Password must be at most 18 characters long"),
   }),
 });
+
+export const ChangePasswordValidationSchema = z.object({
+  body: z.object({
+    oldPassword: z
+      .string()
+      .min(6, "Old password must be at least 6 characters long"),
+    newPassword: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .min(6, { message: "Password must be at least 6 characters long" })
+      .max(18, { message: "Password must not exceed 18 characters" })
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,18}$/,
+        {
+          message:
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+        }
+      ),
+  }),
+});
